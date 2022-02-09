93b3ae9bd26b3093ad7c729fa1e0b04373a17c08
Deepak Kumar Mishra <demishra@microsoft.com>
2022-02-09 08:02:32 +0000

    completed project with all changes

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Item> items = _context.Items.ToList();
+
+            return View("Index",items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var itemToDelete = _context.Items.FirstOrDefault(x => x.Id == id);
+            _context.Items.Remove(itemToDelete);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
